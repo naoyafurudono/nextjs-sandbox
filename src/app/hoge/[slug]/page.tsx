d6dceb62fa7ce62f26e85c5d507d859cba714f87
Naoya Furudono <naoya-furudono@pepabo.com>
2024-12-11 14:43:00 +0900

    done

---
+++ src/app/hoge/[slug]/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { use } from "react";
+
+export default function Home({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = use(params);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>{slug}</h1>
+    </div>
+  );
+}
